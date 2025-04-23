f30851fb885af341808f341d09737c43e7a5f356
naruto <kiptootitus75@gmail.com>
2025-04-23 21:50:01 +0300

    UPDATES

--- main.java
+++ main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        System.out.println("Yooo, Java is live!");
+        //shopping cart
+        Scanner scanner = new Scanner(System.in);
+        String item;
+        Double price;
+        Integer quantity;
+
+        // Create a shopping cart
+        System.out.println("enter the number of items in the cart");
+        item = scanner.nextLine();
+        System.out.println("enter the price of the item");
+        price = scanner.nextDouble();
+        System.out.println("enter the quantity of the item");
+        quantity = scanner.nextInt();
+
+
+        double total = price * quantity;
+        System.out.println("The total price of " + item + " is: " + total);
+
+
+
+        scanner.close();
     }
 }
